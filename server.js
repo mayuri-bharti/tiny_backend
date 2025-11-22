2fb32fd8691d7945b24565221adc91afaf0c1496
mayuri <dev.mayuribharti@gmail.com>
2025-11-22 13:58:50 -0800

    updated

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
